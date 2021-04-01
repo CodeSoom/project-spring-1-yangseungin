b74a2e1dd3585f06a3ec270b3e656231f5d70229
seungin yang <rhfpdk92@naver.com>
2021-04-02 02:34:49 +0900

    프로필 수정뷰 테스트 추가

--- src/test/java/com/devactivity/user/UserControllerTest.java
+++ src/test/java/com/devactivity/user/UserControllerTest.java
@@ -1,6 +1,8 @@
 package com.devactivity.user;
 
 import com.devactivity.errors.UserNotFoundException;
+import com.devactivity.user.form.ProfileForm;
+import com.github.dozermapper.core.Mapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -28,20 +30,23 @@ class UserControllerTest {
     private static final String NOT_JOIN_USER_NAME = "NotJoinUser";
     @Autowired
     private MockMvc mockMvc;
-
     @MockBean
     private UserService userService;
 
+    @MockBean
+    private Mapper mapper;
+
+    private User user;
+
     @BeforeEach
     void setup() {
-        User user = User.builder()
+        user = User.builder()
                 .login(USER_NAME)
                 .name("seungin yang")
                 .build();
 
-        given(userService.getUser(USER_NAME)).willReturn(user);
-        given(userService.getUser(NOT_JOIN_USER_NAME)).willThrow(new UserNotFoundException(NOT_JOIN_USER_NAME));
-        given(userService.isOwner(any(), eq(user))).willReturn(true);
+
+
 
     }
 
@@ -53,8 +58,14 @@ class UserControllerTest {
         @DisplayName("로그인한 사용자가 자신의 프로필을 조회하면")
         class Context_login_user_view_his_or_her_profile {
 
+            @BeforeEach
+            void setUp() {
+                given(userService.getUser(USER_NAME)).willReturn(user);
+                given(userService.isOwner(any(), eq(user))).willReturn(true);
+            }
+
             @Test
-            @DisplayName("프로필을 수정할 수 있다.")
+            @DisplayName("프로필을 볼 수 있다.")
             void it_edit_profile() throws Exception {
                 mockMvc.perform(get("/profile/{userName}", USER_NAME)
                         .with(oauth2Login()))
@@ -70,10 +81,65 @@ class UserControllerTest {
         @DisplayName("가입한적이 없는 사용자의 프로필을 조회하면")
         class Context_view_profile_of_user_who_has_never_signed_up {
 
+            @BeforeEach
+            void setUp() {
+                given(userService.getUser(NOT_JOIN_USER_NAME)).willThrow(new UserNotFoundException(NOT_JOIN_USER_NAME));
+            }
+
             @Test
-            @DisplayName("프로필을 수정할 수 있다.")
+            @DisplayName("에러 페이지를 보여준다.")
             void it_edit_profile() throws Exception {
                 mockMvc.perform(get("/profile/{userName}", NOT_JOIN_USER_NAME))
+                        .andDo(print())
+                        .andExpect(status().isNotFound())
+                        .andExpect(view().name("error/user-not-found"))
+                        .andExpect(content().string(containsString("없는 유저입니다.")));
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("GET /profileedit 요청은")
+    class Describe_viewProfileUpdate {
+
+        @Nested
+        @DisplayName("로그인한 사용자가 자신의 프로필을 수정하면")
+        class Context_login_user_edit_his_or_her_profile {
+
+            @BeforeEach
+            void setUp() {
+                given(userService.getUser(any())).willReturn(user);
+                given(mapper.map(user, ProfileForm.class)).willReturn(new ProfileForm());
+
+            }
+
+            @Test
+            @DisplayName("프로필을 수정 뷰를 볼 수 있다.")
+            void it_edit_profile() throws Exception {
+                mockMvc.perform(get("/profileedit")
+                        .with(oauth2Login()))
+                        .andDo(print())
+                        .andExpect(status().isOk())
+                        .andExpect(view().name("user/profile-edit"))
+                        .andExpect(model().attributeExists("user"))
+                        .andExpect(model().attributeExists("profileForm"))
+                        .andExpect(content().string(containsString("수정하기")));
+            }
+        }
+
+        @Nested
+        @DisplayName("가입한적이 없는 사용자의 프로필을 수정하려하면")
+        class Context_edit_profile_of_user_who_has_never_signed_up {
+            @BeforeEach
+            void setUp() {
+                given(userService.getUser(any())).willThrow(new UserNotFoundException(NOT_JOIN_USER_NAME));
+            }
+
+            @Test
+            @DisplayName("에러 페이지를 보여준다.")
+            void it_edit_profile() throws Exception {
+                mockMvc.perform(get("/profileedit")
+                        .with(oauth2Login()))
                         .andDo(print())
                         .andExpect(status().isNotFound())
                         .andExpect(view().name("error/user-not-found"))
