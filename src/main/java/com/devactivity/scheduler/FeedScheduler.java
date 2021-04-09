361436813b42861157dd573937247765bdc4c66a
seungin yang <rhfpdk92@naver.com>
2021-04-09 18:06:41 +0900

    피드추가 스케쥴링 추가

---
+++ src/main/java/com/devactivity/scheduler/FeedScheduler.java
@@ -0,0 +1,39 @@
+package com.devactivity.scheduler;
+
+import com.devactivity.feed.FeedService;
+import com.devactivity.user.User;
+import com.devactivity.user.UserRepository;
+import com.rometools.rome.io.FeedException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class FeedScheduler {
+    private final UserRepository userRepository;
+    private final FeedService feedService;
+
+    /**
+     * 6시간마다 피드를 등록합니다.
+     */
+    @Scheduled(cron = "0 0 */6 * * ?")
+    public void feedAddScheduler() {
+        Set<User> AllUserRssNotEmpty = userRepository.findAll()
+                .stream().filter(user -> !user.getRssUrl().isBlank())
+                .collect(Collectors.toSet());
+        AllUserRssNotEmpty.forEach(user -> {
+            try {
+                feedService.createFeed(user);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (FeedException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
